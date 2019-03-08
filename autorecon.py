1c503d7aa9dc46a9533dc8d060f7f42c72856ecf
Nicholas Spagnola <39714515+spagzy@users.noreply.github.com>
2019-03-07 22:00:06 -0600

    Add files via upload

---
+++ autorecon.py
@@ -0,0 +1,52 @@
+import nmap
+import optparse
+import subprocess
+
+ps = nmap.PortScanner()
+def runScan(ipAddress):
+    scanResults = ps.scan(ipAddress, '1-1024', '-sV -A')
+    return scanResults
+
+def getPorts(scanResults):
+    portsDict = []
+    hosts = scanResults['scan'].keys()
+    for host in hosts:
+        ports = scanResults['scan'][host]['tcp'].keys()
+        portsDict.append({host: [x for x in ports]})
+    return portsDict
+
+
+def main():
+    parser = optparse.OptionParser('usage%prog -a <ip address>')
+    parser.add_option('-a', dest='ipaddress', type='string', help='provide ip address')
+    (options, args) = parser.parse_args()
+    if options.ipaddress == None:
+    	print parser.usage
+	exit()
+    ipaddress = options.ipaddress
+    results = runScan(ipaddress)
+    portsList = getPorts(results)
+    for i in portsList:
+	hosts = i.keys()
+	for host in hosts:
+		ports = i[host]
+		for port in ports:
+			portInfo = results['scan'][host]['tcp'][port]
+			product = portInfo['product']
+			state = portInfo['state']
+			version = portInfo['version']
+			name = portInfo['name']
+			print 'port: {}\nproduct: {}\nstate: {}\nversion: {}\nname: {}'.format(port, product, state, version, name)
+			print '\n'
+   		if 80 in ports:
+			print '=' * 50
+			print 'running nikto scan'
+			subprocess.call(['nikto', '-h', 'http://192.168.1.1'])
+			print '=' * 50
+			print 'scan ended'
+
+if __name__ == '__main__':
+    main()
+
+
+
